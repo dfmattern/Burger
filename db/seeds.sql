d64cd5ed16bf2cbf171b6f062026b9bb6b7d8218
Dane <56905214+dfmattern@users.noreply.github.com>
2020-04-02 23:46:30 -0600

    wrote schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Guacamole Bacon Burger", true);
